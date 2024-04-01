090948947521f738d2a3cb0f596497cb839477a2
celdorrui <celdorrui@alum.us.es>
2024-04-01 16:53:48 +0200

    segundo commit

--- IISSI2-IS-Frontend-Owner/App.js
+++ IISSI2-IS-Frontend-Owner/App.js
@@ -1,5 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react'
+import ControlPanelScreen from './src/screens/controlPanel/ControlPanelScreen'
+import ProfileScreen from './src/screens/profile/ProfileScreen'
+import RestaurantsScreen from './src/screens/restaurants/RestaurantsScreen'
+
+const Tab = createBottomTabNavigator()
+
+export default function App () {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurants"
+          component={RestaurantsScreen} />
+        <Tab.Screen
+          name="Control Panel"
+          component={ControlPanelScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default function App() {
   return (
